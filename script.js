41779ba3136afed20c5bd0747d018bb0d888d590
KelahKelah <kelechiekebor@gmail.com>
2020-06-08 12:54:29 +0100

    modified script file and read me

--- script.js
+++ script.js
@@ -8,38 +8,38 @@ script.async = true;
 window.initMap = function() {
      shop = {lat: 6.4595445, lng: 7.4931562}
      // add a new map object
-     var map = new google.maps.Map(document.getElementById("map"), {center: {lat: 6.4595445, lng: 7.4931562}, zoom: 14 });
+     var map = new google.maps.Map(document.getElementById("map"), {center: {lat: 6.4595445, lng: 7.4931562}, zoom: 12 });
      
 var marker = [
      {
-          coords: {lat:6.4515293, lng: 7.5122126},
-          content: '<h2>Shoprite</h2>',
-          map: map
-     },
-     {
-          coords: {lat:6.4494885, lng: 7.490644},
-          content: '<h2>Ogbete</h2>',
+          coords: {lat:6.4595445, lng: 7.4931562},
+          content: '<h2>BS001</h2><p>Shoprite</p>',
           map: map
      },
      {
           coords: {lat:6.4354518, lng: 7.4826216},
-          content: '<h2>Centenary City</h2>',
+          // content: '<h2>BS002</h2><p>Ogbete</p>',
           map: map
      },
      {
-          coords: {lat:6.459964, lng: 7.548949},
-          content: '<h2>Bethel Plaza</h2>',
+          coords: {lat:6.3908948, lng: 7.5318491}, 
+          content: '<h2>BS003</h2><p>Centenary City</p>',
+          map: map
+     },
+     {
+          coords: {lat:6.4494885, lng: 7.490644}, 
+          content: '<h2>BS004</h2><p>Bethel Plaza</p>',
           map: map
      },
      {
           coords: {lat:6.4553312, lng: 7.5021473},
-          content: '<h2>Elim Plaza</h2>',
+          content: '<h2>BS005</h2><p>Elim Plaza</p>',
           map: map
      }
      
 ]
-     // loop through markers
-     for(i=0; i< marker.length; i++) {
+     // loop through marker array
+     for(i = 0; i < marker.length; i++) {
           addMarker(marker[i]); 
      }   
           // add a new marker object
@@ -54,15 +54,20 @@ var marker = [
                // if(iconImage) {
                //      marker.setIcon(iconImage)
                // }
-          
+
+               // Checking  for content
                if(props.content) {
                     var infoWindow = new google.maps.InfoWindow(
                          {content: props.content }
                     );
                     marker.addListener('click', function(event) {
                          infoWindow.open(map, marker);
+                         // console.log('my event', event)
+
                     })
-                    console.log('my event', event)
+               } else {
+                    console.log('no content to display')
+                    
                }
      }
      
